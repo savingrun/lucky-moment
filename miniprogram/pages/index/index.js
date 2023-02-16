eda36a8a63d502d4b8858054f6426f078014d59e
saving <savingrun@gmail.com>
2023-02-17 00:52:00 +0800

    init

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,220 @@
+// index.js
+// const app = getApp()
+const {
+    envList
+} = require('../../envList.js');
+import Toast, {
+    hideToast
+} from 'tdesign-miniprogram/toast/index';
+Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {},
+    onLoad(options) {
+        this.setData({
+            envId: options.envId
+        });
+    },
+});
+
+Component({
+    data: {
+        genshinDataFlag: true,
+        contentUrl: '',
+        ssrList: [0],
+        tabPanelstyle: 'display:flex; justify-content:center; align-items:center; min-height:120px;',
+        copyright: 'Saving © 2021-2031 TD.All Rights Reserved.',
+        rolePoolList: [],
+        weaponPoolList: [],
+        permanentPoolList: [],
+        dynamicMessages: ''
+    },
+    methods: {
+        onShareAppMessage() {
+            return {
+                title: '欢迎欧皇',
+                path: 'pages/index/index',
+                imageUrl: ''
+            }
+        },
+
+        onTabsChange(event) {
+            console.log(`Change tab, tab-panel value is ${event.detail.value}.`);
+        },
+
+        onTabsClick(event) {
+            console.log(`Click tab, tab-panel value is ${event.detail.value}.`);
+        },
+
+        contentUrl(e) {
+            console.log(e.detail.value)
+            var that = this
+            that.setData({
+                contentUrl: e.detail.value.replace(/\s+/g, '')
+            })
+        },
+
+        getUrlParam(contentUrl, paramName) {
+            let search = contentUrl.replace(/\?/, '')
+            let reg = new RegExp('(^|&)' + paramName + '=(.*?)(&|$)');
+            let res = search.match(reg);
+            return res ? res[2] : null;
+        },
+
+        analyse(e) {
+            var that = this;
+            console.log(that.data.contentUrl)
+            var authKey = that.getUrlParam(that.data.contentUrl, "authkey")
+            console.log(authKey)
+            var endId = 0
+            var page = 1
+            let size = 20
+            let gachaTypeList = [301, 302, 200]
+            var baseUrl = "https://hk4e-api.mihoyo.com/event/gacha_info/api/getGachaLog?win_mode=fullscreen&authkey_ver=1&sign_type=2&auth_appid=webview_gacha&init_type=301&timestamp=1673997960&lang=zh-cn&device_type=mobile&plat_type=ios&region=cn_gf01&game_biz=hk4e_cn" +
+                "&authkey=" + authKey +
+                "&size=" + size +
+                "&gacha_type=301"
+            console.log(baseUrl)
+            let dynamicMessagesBefore = '分析第 '
+            let dynamicMessagesAfter = ' 页数据中...'
+            that.setData({
+                dynamicMessages: dynamicMessagesBefore + page + dynamicMessagesAfter
+            })
+            Toast({
+                context: that,
+                selector: '#t-toast',
+                theme: 'loading',
+                direction: 'column',
+                duration: 120000,
+                preventScrollThrough: true,
+            });
+            var intervalId = setInterval(function () {
+                var currentUrl = baseUrl +
+                    "&timestamp=" + Date.parse(new Date()) / 1000 +
+                    "&page=" + page +
+                    "&end_id=" + endId
+                console.log(page)
+                wx.request({
+                    url: currentUrl,
+                    header: {
+                        'Host': 'hk4e-api.mihoyo.com',
+                        'Origin': 'https://webstatic.mihoyo.com',
+                        'Cookie': 'mi18nLang=zh-cn',
+                        'Connection': 'keep-alive',
+                        'Accept': 'application/json, text/plain, */*',
+                        // 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+                        'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
+                        'Referer': 'https://webstatic.mihoyo.com/',
+                        'Accept-Encoding': 'gzip, deflate, br'
+                    },
+                    timeout: 2000,
+                    dataType: 'json',
+                    method: 'GET',
+                    success(res) {
+                        var reply = res.data
+                        console.log(reply)
+                        if (reply.retcode == -101) {
+                            that.setData({});
+                            Toast({
+                                context: that,
+                                selector: '#t-toast-warning',
+                                message: reply.message,
+                                theme: 'warning',
+                                direction: 'column',
+                            });
+                        } else if (reply.retcode == -100) {
+                            that.setData({});
+                            Toast({
+                                context: that,
+                                selector: '#t-toast-warning',
+                                message: reply.message,
+                                theme: 'warning',
+                                direction: 'column',
+                            });
+                        } else {
+                            console.log(reply.data.list.length)
+                            that.setData({
+                                uid: 'UID: ' + reply.data.list[0].uid,
+                                rolePoolList: that.data.rolePoolList.concat(reply.data.list),
+                                genshinDataFlag: false,
+                                dynamicMessages: dynamicMessagesBefore + page + dynamicMessagesAfter
+                            });
+                            var initialValue = 0
+                            if (page >= 2) {
+                                initialValue = page * size
+                            }
+                            // for(let i = 0; i < reply.data.list.length; i++) {
+                            //     let cur = reply.data.list[i]
+                            //     console.log("initialValue:" + initialValue)
+                            //     if(cur.rank_type == '5') {
+                            //         console.log("====")
+                            //         console.log(cur)
+                            //         var list = that.data.ssrList
+                            //         console.log(list)
+                            //         list.push(i + initialValue)
+                            //         console.log(list)
+                            //         that.setData({
+                            //             ssrList: list
+                            //         });
+                            //     }
+                            // }
+                            page++
+                            endId = reply.data.list[reply.data.list.length - 1].id
+                            if (reply.data.list.length < reply.data.size) {
+                                clearInterval(intervalId)
+                            }
+                        }
+                    },
+                    fail(err) {},
+                    complete(res) {
+                        var reply = res.data
+                        if (reply.data.list.length < reply.data.size) {
+                            hideToast({
+                                context: that,
+                                selector: '#t-toast',
+                            });
+                        }
+                    }
+                })
+            }, 1200)
+
+            // }
+
+
+
+            // wx.cloud.callFunction({
+            //     name: 'analyse',
+            //     config: {
+            //         env: this.data.envId
+            //     },
+            //     data: {
+            //         url: this.data.contentUrl
+            //     }
+            // }).then((resp) => {
+            //     console.log(resp.result)
+            //     if (resp.result.retcode == -101) {
+            //         this.setData({});
+            //     } else if (resp.result.retcode == -100) {
+            //         this.setData({});
+            //     } else {
+            //         this.setData({
+            //             uid: 'UID: ' + resp.result.data.list[0].uid,
+            //             genshinData: resp.result.data.list,
+            //             genshinDataFlag: false
+            //         });
+            //     }
+            //     hideToast({
+            //         context: this,
+            //         selector: '#t-toast',
+            //     });
+            // }).catch((e) => {
+            //     hideToast({
+            //         context: this,
+            //         selector: '#t-toast',
+            //     });
+            // });
+        },
+
+    },
+});
